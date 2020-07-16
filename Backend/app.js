c33d3829d4b737cc5490e2286f1243ef465f551c
Siva <karthikgamidi@gmail.com>
2020-07-16 18:13:00 +0530

    CORS error resolved

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const userRoutes = require('./routes/user');
 const app = express();
@@ -13,7 +14,13 @@ mongoose.connect('mongodb+srv://',
 });
 
 app.use(bodyParser.json());
-
+app.use(cors());
+/*app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Headers');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});*/
 app.use("/api/user", userRoutes);
 
 module.exports = app;
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -36,36 +36,36 @@ routes.post('/signup', (req, res, next) => {
 
 routes.post('/login', (req, res, next) => {
     let userData;
-    if(req.body.email && req.body.password){
-        User.findOne({email: req.body.email}).then(response => {
-            if(!response){
+    if (req.body.email && req.body.password) {
+        User.findOne({ email: req.body.email }).then(response => {
+            if (!response) {
                 return res.status(200).json({
                     rc: '1',
                     message: 'Email not found'
                 });
             }
             userData = response;
-            return bcrypt.compare(req.body.password, userData.password)
-        }).then(data => {
-            if(!data){
-                return res.status(200).json({
-                    rc: '1',
-                    message: 'Auth failed'
+            bcrypt.compare(req.body.password, userData.password).then(data => {
+                if (!data) {
+                    return res.status(200).json({
+                        rc: '1',
+                        message: 'Auth failed'
+                    });
+                }
+                res.status(200).json({
+                    rc: '0',
+                    message: "Authenticated successfully"
                 });
-            }
-            res.status(200).json({
-                rc: '0',
-                message: "Authenticated successfully"
-            });
-        }).catch(err => {
-            return res.status(200).json({
-                rc: '1',
-                message: 'Something went wrong'
+            }).catch(err => {
+                res.status(200).json({
+                    rc: '1',
+                    message: 'Something went wrong'
+                });
             });
         })
     }
-    else{
-        res.status(200).json({
+    else {
+        return res.status(200).json({
             rc: '1',
             message: 'Invalid Data'
         });
