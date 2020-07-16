8e153f8612a91af0d3ec9c6d10032dee0d5a19a6
Siva <karthikgamidi@gmail.com>
2020-07-16 10:36:18 +0530

    Mongo

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 const app = express();
 
-mongoose.connect('mongodb+srv://siva:Karthik%401@cluster0.lykgc.mongodb.net/angular-emp?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://',
 { useNewUrlParser: true, useCreateIndex: true }).then(() => {
     console.log("DB connection successful");
 }).catch(err => {
